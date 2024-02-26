e17f05e54863348fd7a7e954c53a5c282bd71626
Ishaq <isez9805@gmail.com>
2024-02-26 15:04:02 +0100

    feat #2: implemented binary search

---
+++ src/main/Search.java
@@ -0,0 +1,24 @@
+package src.main;
+
+import java.util.ArrayList;
+
+public class Search {
+
+
+    public int BinarySearch(ArrayList<Integer> A, int key) {
+        int l = 0;
+        int r = A.size() ;
+        while(l <= r) {
+            int x = (l + r) / 2;
+            if(A.get(x) < key) {
+                l = x + 1;
+            } else if(A.get(x) > key) {
+                r = x - 1;
+            } else {
+                return x;
+            }
+        }
+        return -1; // Key not found
+    }
+
+}
